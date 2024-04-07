7289b62e3d8a693a45cb3820c8382fe4ec5c73e3
Arjun kumar vs <arjun.tech177@gmail.com>
2024-04-07 22:14:34 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children})=>{
     const { authUser } = useAuthContext()
     useEffect(()=> {
   if(authUser){
-    const socket = io("http://localhost:8000", {
+    const socket = io("https://mern-chat-app-hocu.onrender.com", {
         query: {
             userId: authUser._id
         }
